3a8ae02a1c5ed6c9a4a3a4b50ad8c8f778af69d0
marcinoles87 <marcinoles87@gmail.com>
2023-10-07 18:28:08 +0200

    work with nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,34 @@
 import React from 'react'
+  import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 function Header() {
   return (
-    <div>Header</div>
+
+    
+    <Container className='header'>
+      <Row>
+        <Col className='col-one'>
+        
+        <button>Take a litle time to read...</button>
+        <h1>Hi I'm Marcin Web Developer</h1>
+        <p>lorem ipsum lorem ipsum lorem ipsum
+        lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum
+        lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum'
+        lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum
+        </p>
+        </Col>
+
+        <Col className='col-two'>
+        <img src='' alt='logo-header'></img>
+        
+        </Col>
+      </Row>
+    </Container>
+        
+    
   )
 }
 
