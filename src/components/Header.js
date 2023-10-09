f084342d916c956eed068e13ac8f311f7e8fe77d
marcinoles87 <marcinoles87@gmail.com>
2023-10-09 05:33:35 +0200

    work with header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,24 +2,26 @@ import React from 'react'
   import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Astro from '../img/ast.png'
+import Astro from '../img/ast.png';
+import { ArrowRightCircle} from 'react-icons'
 
 
 function Header() {
   return (
 
     
-    <Container className='header'>
-      <Row>
-        <Col className='col-one'>
+    <Container className='banner'>
+      <Row className='align-items-center'>
+        <Col xs={12} xl={7} md={6}>
         
-        <button>Take a litle time to read...</button>
-        <h1 className='header_animated_text'>Hi I'm Marcin Web Developer</h1>
-        <p>lorem ipsum lorem ipsum lorem ipsum
+        <span className='tagline'>Welcome to my portfolio</span>
+        <h1>Hi I'm Marcin Web Developer</h1>
+        <span>lorem ipsum lorem ipsum lorem ipsum
         lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum
         lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum'
         lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum
-        </p>
+        </span>
+        <button onClick={ () =>  console.log('connect')}>Lets connect </button>
         </Col>
 
         <Col className='col-two'>
