116903a2266828849e83738ac8a39842c1ec1947
marcinoles87 <marcinoles87@gmail.com>
2023-10-13 05:41:24 +0200

    work with skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,8 +1,50 @@
 import React from 'react'
+import Col from 'react-bootstrap/esm/Col';
+import Row from 'react-bootstrap/esm/Row';
+import Container from 'react-bootstrap/esm/Container';
+   import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
 
 function Skills() {
-  return (
-    <div>Skills</div>
+
+ 
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+};
+
+return(
+<section className='skill' id='skills'>
+    <Container>
+        <Row>
+            <Col>
+            <div className='skill-bx'>
+                <h2>Skills</h2>
+                <p>jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkusz</p>
+                <Carousel responsive={3}>
+                    
+                </Carousel>
+            </div>
+            </Col>
+        </Row>
+    </Container>
+</section>
   )
 }
 
