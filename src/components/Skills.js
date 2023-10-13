ac26200245b6c254e86349d8c7e707913dcae663
marcinoles87 <marcinoles87@gmail.com>
2023-10-13 05:42:14 +0200

    work with skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -37,7 +37,7 @@ return(
             <div className='skill-bx'>
                 <h2>Skills</h2>
                 <p>jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkusz</p>
-                <Carousel responsive={3}>
+                <Carousel responsive={responsive} infinite={true}>
                     
                 </Carousel>
             </div>
