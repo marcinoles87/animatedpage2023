1ec187fd96fb4f668e45bc0669e42ad0ae002b06
marcinoles87 <marcinoles87@gmail.com>
2023-10-17 16:00:47 +0200

    work with projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,6 +5,7 @@ import Container from 'react-bootstrap/esm/Container'
 import Row from 'react-bootstrap/esm/Row'
 import Col from 'react-bootstrap/esm/Col'
 import Navbar from 'react-bootstrap/esm/Nav'
+import Nav from 'react-bootstrap/Nav'
 import NavItem from 'react-bootstrap/esm/NavItem'
 import NavLink from 'react-bootstrap/esm/NavLink'
 
@@ -54,17 +55,18 @@ function Projects() {
                 <Col>
                     <h2>Projects</h2>
                     <p>Visual Studio Code comes with Emmet preinstalled. Emmet is a plugin that helps you write HTML and CSS easier using shortcuts.Thanks to Emmet it is really easy to generate lorem ipsum. You no longer have to search for a lorem ipsum online generator.</p>
-                    <Navbar expand="lg" className="bg-body-tertiary">
-                        <NavItem>
-                            <NavLink>Active</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink>New</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink>Oldest</NavLink>
-                        </NavItem>
-                    </Navbar>
+                    
+                    <Nav variant="pills" defaultActiveKey="/home" justify={true}>
+                        <Nav.Item>
+                            <Nav.Link eventKey="first">Active</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="second">Option 2</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="third">Option 3 </Nav.Link>
+                        </Nav.Item>
+                    </Nav>
                 
 
                 </Col>
