98befa3e25f52a63352c77be35aeb1b1b51389c1
marcinoles87 <marcinoles87@gmail.com>
2023-10-18 16:49:52 +0200

    work with projects section

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,7 +3,7 @@ import Col from 'react-bootstrap/esm/Col'
 
 function ProjectCard({title , description , imgUrl}) {
   return (
-    <Col sm={12} md={4} xs={12}>
+    <Col sm={6} md={4} xs={12}>
         <div className='card-img'>
             <img src={imgUrl} alt={title}></img>
             <h4>{description}</h4>
