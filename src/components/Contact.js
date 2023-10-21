cf90124db983942e65e08fe140689da17fb82aaf
marcinoles87 <marcinoles87@gmail.com>
2023-10-21 09:21:30 +0200

    work with contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,9 @@
 
 import {useState} from 'react';
+import Container from 'react-bootstrap/esm/Container';
+import Row from 'react-bootstrap/esm/Row';
+import Col from 'react-bootstrap/Col';
+import imgContact from '../img/contact.jpg'
 
 export const Contact = () => {
     const formInitialDetails = {
@@ -15,7 +19,21 @@ export const Contact = () => {
     const [status , setStatus] = useState({})
     return(
      <section className='contact' id='contact'>
-        
+        <Container>
+            <Row className='align-items-center'>
+                <Col md={6}>
+                    <img src={imgContact} alt='contact-img'></img>
+                </Col>
+                <Col md={6}>
+                    <h2> Get in Touch</h2>
+                    <form>
+                        <Row>
+                            <Col sm={6} className='px-1'></Col>
+                        </Row>
+                    </form>
+                </Col>
+            </Row>
+        </Container>
      </section>
 
     )
